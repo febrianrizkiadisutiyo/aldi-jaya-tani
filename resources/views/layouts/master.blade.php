a3bcaca105b194a2e5bf7e5a85ffb362ba142c49
febrianrizkiadisutiyo <12050116997@students.uin-suska.ac.id>
2022-12-17 10:40:05 +0700

    commit

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -77,7 +77,7 @@
                 <li>
                     <a href="#2" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                         <i class="bi bi-wallet2 mr-2"></i>
-                        Kelola produk
+                        Kelola Produk
                     </a>
                     <ul class="collapse" id="2">
 
